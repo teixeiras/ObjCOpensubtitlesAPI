d37320756b289f663ba0c4f9b8c7ff7a17f28ea9
Filipe Teixeira <teixeiras@gmail.com>
2014-07-03 22:37:48 +0100

    Support to Download Subtitles Remove Categories

---
+++ OpensubtitleAPI/Requests/DownloadSubtitleHandler.h
@@ -0,0 +1,13 @@
+//
+//  DownloadSubtitleHandler.h
+//  OpensubtitleAPI
+//
+//  Created by Filipe Teixeira on 03/07/14.
+//  Copyright (c) 2014 Filipe Teixeira. All rights reserved.
+//
+
+#import <OpensubtitleAPI/OpensubtitleAPI.h>
+
+@interface DownloadSubtitleHandler : RequestHandler
+
+@end
---
+++ OpensubtitleAPI/Requests/DownloadSubtitleHandler.m
@@ -0,0 +1,13 @@
+//
+//  DownloadSubtitleHandler.m
+//  OpensubtitleAPI
+//
+//  Created by Filipe Teixeira on 03/07/14.
+//  Copyright (c) 2014 Filipe Teixeira. All rights reserved.
+//
+
+#import "DownloadSubtitleHandler.h"
+
+@implementation DownloadSubtitleHandler
+
+@end
